a63813e8ae4c22fae0cf105bee9f220b32f9f069
Eslam-Sayed7 <147099167+Eslam-Sayed7@users.noreply.github.com>
2023-11-14 16:55:40 +0200

    Update run.cpp with some commments

--- Task-2/run.cpp
+++ Task-2/run.cpp
@@ -1,3 +1,15 @@
+// FCAI - OOP Programming - 2023 - Assignment 2
+// Program Name: ------------------------------.cpp
+// Last Modification Date:  19/10/2023
+// Author1 and ID and Group:  Abdullah Saeed Alnami  20220705  bdallhsydalnmy@gmail.com Section 9
+// Author2 and ID and Group:  Eslam Sayed Younus     20220495  eslam894408@gmail.com    Section 9
+// Author3 and ID and Group:  Youssef Hossam Ahmed   20220390  y.hossam2274@gmail.com   Section 9
+// Teaching Assistant:    xxxxx xxxxx
+
+
+// Purpose: Making a data type called Bigreal which can sotre and make different operations
+// on large values that c++ standard data types for numbers can do
+
 #include "BigReal.cpp"
 using namespace std;
 
